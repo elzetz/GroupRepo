2091bbf4c9cee4a0ab021e7b268ffffeced2ffa3
elzetz <79876315+elzetz@users.noreply.github.com>
2021-03-02 06:16:24 +0300

    second task

--- LisaTest2.py
+++ LisaTest2.py
@@ -1,7 +1,21 @@
 newlist = []
 isNotNull = True
-while isNotNull = True:
-    #Put some code here:
-    #A) How to return from the cycle
-    #B) Input function
-    #C)output array, min and max
+# noinspection PyStatementEffect
+while isNotNull == True:
+    new_value = int(input("a number here"))
+    if new_value == 0:
+        isNotNull = False
+    else:
+        newlist.append(new_value)
+
+
+print("There is entered array:", newlist)
+
+
+print(min(newlist), "is the minimal value")
+
+
+print(max(newlist), "is the maximal value")
+
+
+print(max(newlist)-min(newlist), "is the difference")
